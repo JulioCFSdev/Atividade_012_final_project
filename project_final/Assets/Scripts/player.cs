94718886eb7725b86678ad56023abeb4f1d3336d
Guilherme Correia Tapajós Araújo <gcta.snf21@uea.edu.br>
2022-05-11 14:28:15 -0400

    Beginning of Boss level and animations

--- project_final/Assets/Scripts/player.cs
+++ project_final/Assets/Scripts/player.cs
@@ -12,11 +12,13 @@ public class player : MonoBehaviour
     public bool doubleJump;
 
     private Rigidbody2D rig;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +32,25 @@ public class player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+        
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+           anim.SetBool("walk", true); 
+           transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+           anim.SetBool("walk", true);
+           transform.eulerAngles = new Vector3(0f, 180f, 0f); 
+        }
+
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+           anim.SetBool("walk", false);
+        }
+
+
     }
 
     void Jump()
@@ -40,7 +61,8 @@ public class player : MonoBehaviour
             if (!isJumping)
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                doubleJump = true;            
+                doubleJump = true;
+                anim.SetBool("jump", true);            
             }
 
             else
@@ -60,9 +82,10 @@ public class player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 6)
+        if (collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
         {
             isJumping = false;
+            anim.SetBool("jump", false);
         }
 
 
@@ -70,7 +93,7 @@ public class player : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 6)
+        if (collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
         {
             isJumping = true;
         }
