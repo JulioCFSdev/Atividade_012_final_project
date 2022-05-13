f96a36c169fff4e832610c184006e9f3d7f71874
Guilherme Correia Tapajós Araújo <gcta.snf21@uea.edu.br>
2022-05-13 10:55:50 -0400

    Enemy's death

--- project_final/Assets/Scripts/Toadman_FB.cs
+++ project_final/Assets/Scripts/Toadman_FB.cs
@@ -10,7 +10,12 @@ public class Toadman_FB : MonoBehaviour
 
     public float speed;
 
+    public Transform headPoint;
+
     private Animator anim;
+
+    public BoxCollider2D boxCollider2D;
+    public CircleCollider2D circleCollider2D;
 
 
     // Start is called before the first frame update
@@ -36,11 +41,37 @@ public class Toadman_FB : MonoBehaviour
         }
     }
 
+     bool playerDestroyed = false;
      void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "static_trap")
         {
             Destroy(gameObject);
+        }
+
+        if(collision.gameObject.tag == "Player")
+        {
+            
+            float height = collision.contacts[0].point.y - headPoint.position.y;
+            Debug.Log(height);
+
+            if(height > 0 && !playerDestroyed)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                speed = 0;
+                anim.SetTrigger("die");
+                Debug.Log("foi");
+                boxCollider2D.enabled = false;
+                circleCollider2D.enabled = false;
+                rig.bodyType = RigidbodyType2D.Kinematic;
+                Destroy(gameObject, 0.33f);
+            }
+            else
+            {
+                playerDestroyed = true;
+                GameController.instance.ShowGameOver();
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
