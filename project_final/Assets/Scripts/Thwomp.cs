0a6c41bff1d7c7b74edb4cc56d2586b65c0b9c1f
Johann Mayos(요한) <42842871+johannmay@users.noreply.github.com>
2022-05-12 20:51:13 -0400

    Traps Added

--- project_final/Assets/Scripts/Thwomp.cs
+++ project_final/Assets/Scripts/Thwomp.cs
@@ -1,67 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Thwomp : player
+public class Thwomp : MonoBehaviour
 {
-    [Header("SpikeHead Attributes")]
-    [SerializeField] private float speed;
-    [SerializeField] private float range;
-    [SerializeField] private float checkDelay;
-    [SerializeField] private LayerMask playerLayer;
-    private Vector3[] directions = new Vector3[4];
-    private Vector3 destination;
-    private float checkTimer;
-    private bool attacking;
+    public GameObject Block;
+    public GameObject hit;
+    private bool isMoving;
+    public float speed;
 
-    private void OnEnable()
+    // Update is called once per frame
+    void Update()
     {
-        Stop();
-    }
-    private void Update()
-    {
-        //Move spikehead to destination only if attacking
-        if (attacking)
-            transform.Translate(destination * Time.deltaTime * speed);
-        else
-        {
-            checkTimer += Time.deltaTime;
-            if (checkTimer > checkDelay)
-                CheckForPlayer();
+        if(isMoving == true){
+            Block.transform.position = Vector2.MoveTowards(Block.transform.position, hit.transform.position, speed * Time.deltaTime);
         }
     }
-    private void CheckForPlayer()
-    {
-        CalculateDirections();
 
-        //Check if spikehead sees player in all 4 directions
-        for (int i = 0; i < directions.Length; i++)
-        {
-            Debug.DrawRay(transform.position, directions[i], Color.red);
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, directions[i], range, playerLayer);
 
-            if (hit.collider != null && !attacking)
-            {
-                attacking = true;
-                destination = directions[i];
-                checkTimer = 0;
-            }
+    private void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.CompareTag("Player")){
+            Block.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            Block.GetComponent<Rigidbody2D>().gravityScale = 7;
+            Block.GetComponent<Rigidbody2D>().mass = 400;
+
         }
     }
-    private void CalculateDirections()
-    {
-        directions[0] = transform.right * range; //Right direction
-        directions[1] = -transform.right * range; //Left direction
-        directions[2] = transform.up * range; //Up direction
-        directions[3] = -transform.up * range; //Down direction
-    }
-    private void Stop()
-    {
-        destination = transform.position; //Set destination as current position so it doesn't move
-        attacking = false;
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Ground")){
+            Block.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            Block.GetComponent<Rigidbody2D>().gravityScale = 0;
+            Block.GetComponent<Rigidbody2D>().mass = 0;
+
+            isMoving = true;
+        }
+
     }
 
-    private void OnTriggerEnter2D(Collider2D fall)
-    {
-        
-        Stop(); //Stop spikehead once he hits something
-    }
+
 }
