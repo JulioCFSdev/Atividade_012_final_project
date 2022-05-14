3ba0039f3b1bb53de12b32dd71cfeed722dda59b
JulioCFSdev <90019843+JulioCFSdev@users.noreply.github.com>
2022-05-14 02:19:59 -0300

    Adding Lvl_2 and flag and falling  platform prefabs

---
+++ project_final/Assets/Scripts/plataform.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataform : MonoBehaviour
+{
+    public float fallingTime;
+
+    private TargetJoint2D target;
+    private BoxCollider2D boxColl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GetComponent<TargetJoint2D>();
+        boxColl = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Invoke("Falling", fallingTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "static_trap")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Falling()
+    {
+        target.enabled = false;
+        boxColl.isTrigger = true;
+    }
+}
